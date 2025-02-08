6e38a260dd30a421f52c3861021ef21a8eb9042e
yummygyudon <bang2brew@gmail.com>
2025-02-08 20:34:13 +0900

    init commit

---
+++ src/main/java/yummy_ground/yummygyudon/obms/ObmsApplication.java
@@ -0,0 +1,13 @@
+package yummy_ground.yummygyudon.obms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yummy_ground/yummygyudon/obms/ObmsApplicationTests.java
@@ -0,0 +1,13 @@
+package yummy_ground.yummygyudon.obms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
