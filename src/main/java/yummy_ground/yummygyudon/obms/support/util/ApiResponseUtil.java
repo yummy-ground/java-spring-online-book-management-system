db03ae0491b8a5beb25c3272f0c0e4cd2c23bbcb
yummygyudon <bang2brew@gmail.com>
2025-02-11 23:40:23 +0900

    refactor : Add Method generate NoContent Response & Method generate Error Response with message in `ApiResponseUtil`
    Validation 라이브러리에서 발생하는 `MethodArgumentNotValidException`의 경우, 의도한 ErrorCode를 가지지 않으면서 서로다른 메시지를 가지기 때문에 ExceptionHandler에서 일관된 ApiResponse를 생성하기 위해 메서드를 추가했습니다.

--- src/main/java/yummy_ground/yummygyudon/obms/support/util/ApiResponseUtil.java
+++ src/main/java/yummy_ground/yummygyudon/obms/support/util/ApiResponseUtil.java
@@ -1,6 +1,7 @@
 package yummy_ground.yummygyudon.obms.support.util;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import yummy_ground.yummygyudon.obms.support.code.ErrorCode;
@@ -11,19 +12,27 @@ import static lombok.AccessLevel.PRIVATE;
 
 @RequiredArgsConstructor(access = PRIVATE)
 public final class ApiResponseUtil {
-    public static <T> ResponseEntity<BaseResponse<?>> success(SuccessCode code, T data) {
+
+    public static <T> ResponseEntity<BaseResponse<?>> successContent(SuccessCode code, T data) {
         return ResponseEntity
                 .status(code.getStatus())
                 .body(BaseResponse.of(code.getMessage(), data));
     }
-    public static ResponseEntity<BaseResponse<?>> success(SuccessCode code) {
-        return ResponseEntity
-                .status(code.getStatus())
-                .body(BaseResponse.of(code.getMessage()));
+
+    public static ResponseEntity<BaseResponse<?>> successNoContent() {
+        return ResponseEntity.noContent().build();
     }
+
     public static ResponseEntity<BaseResponse<?>> fail(ErrorCode code) {
         return ResponseEntity
                 .status(code.getStatus())
                 .body(BaseResponse.of(code.getMessage()));
     }
+
+    public static ResponseEntity<BaseResponse<?>> fail(String message) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST.value())
+                .body(BaseResponse.of(message));
+    }
+
 }
